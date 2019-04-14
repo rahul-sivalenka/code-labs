e6d2ae59caa2b29a00b9c5d9290806eb180176cc
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-04-15 00:26:58 +0530

    (Learn-React) Coverted Square component to stateless func

--- react/intro/src/index.js
+++ react/intro/src/index.js
@@ -2,25 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value
-        };
-    }
+// class Square extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             value: props.value
+//         };
+//     }
 
-    render() {
-        return (
-            <button className="square" onClick={() => this.setState({ value: 'X' })}>{this.state.value}</button>
-        );
-    }
+//     render() {
+//         return (
+//             <button className="square" 
+//                     onClick={() => this.setState({ value: 'X' })}>
+//                 {this.state.value}
+//             </button>
+//         );
+//     }
+// }
+
+function Square (props) {
+    return (
+        <button className="square" 
+                onClick={() => props.onClick()}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null)
+        };
     }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} 
+                       onClick={() => this.handleClick(i)} />
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({
+            squares
+        });
+    }
+
     render() {
         return (
             <div>
