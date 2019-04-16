708f0281cf0938dfd993e61da0481a5dcfd9dec1
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-04-16 14:14:25 +0530

    (PS-React) Completed add new task action. Introduced separate reducers

--- react/ps-react-express-app/src/app/store/index.js
+++ react/ps-react-express-app/src/app/store/index.js
@@ -1,18 +1,39 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { defaultState } from '../../server/defaultState';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 import * as sagas from './sagas.mock';
+import * as mutations from './mutations';
 
 export const store = createStore(
-  function reducer(state = defaultState, action) {
-    return state;
-  },
+  combineReducers({
+    tasks(tasks = defaultState.tasks, {type, taskId, groupId, ownerId}) {
+      switch(type) {
+        case mutations.CREATE_TASK:
+          return [...tasks, {
+            id: taskId,
+            name: 'New Task',
+            group: groupId,
+            owner: ownerId,
+            isComplete: false
+          }]
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments, action) {
+      return comments;
+    },
+    groups(groups = defaultState.groups, action) {
+      return groups;
+    },
+    users(users = defaultState.users, action) {
+      return users;
+    }
+  }),
   applyMiddleware(createLogger(), sagaMiddleware)
 );
-
 
 for (let saga in sagas) {
   sagaMiddleware.run(sagas[saga]);
--- react/ps-react-express-app/src/app/store/sagas.mock.js
+++ react/ps-react-express-app/src/app/store/sagas.mock.js
@@ -16,6 +16,9 @@ import uuid from 'uuid';
 export function* taskCreationSaga() {
   while(true) {
     const {groupId} = yield take(mutations.REQUEST_TASK_CREATION);
+    const ownerId = `U1`;
+    const taskId = uuid();
+    yield put(mutations.createTask(taskId, groupId, ownerId));
     console.log('Got group ID', groupId);
   }
 }
