3a2d3c5a9912b4dc664cd2da7f59417508aabaa8
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-04-16 12:59:39 +0530

    (PS-React) Connected Redux to Dashboard component

--- react/ps-react-express-app/src/app/components/Dashboard.jsx
+++ react/ps-react-express-app/src/app/components/Dashboard.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 export const Dashboard = ({groups}) => (
   <div>
     <h2>Dashboard</h2>
   </div>
 );
+
+function mapStateToProps(state) {
+  const { groups } = state;
+  return {
+    groups
+  };
+}
+
+export const ConnectedDashboard = connect(mapStateToProps)(Dashboard);
--- react/ps-react-express-app/src/app/components/Main.jsx
+++ react/ps-react-express-app/src/app/components/Main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
-import { Dashboard } from './Dashboard';
+import { ConnectedDashboard } from './Dashboard';
 
 export const Main = () => (
   <Provider store={store}>
     <div>
-      <Dashboard />
+      <ConnectedDashboard />
     </div>
   </Provider>
 );
